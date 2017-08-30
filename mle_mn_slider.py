bf2f931aa4030083c761980542e5bac4a5c22440
Benjamin L Melosh <bmelosh@usgs.gov>
2017-08-30 15:56:36 -0700

    working in bokeh plots

--- mle_mn_slider.py
+++ mle_mn_slider.py
@@ -7,28 +7,27 @@ from bokeh.io import curdoc
 from bokeh.layouts import column, widgetbox
 from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Slider, TextInput
-from bokeh.plotting import figure
+from bokeh.plotting import figure, show
 
 
 #Function to import MLE data and create a dataframe with MLE data for all TRIBUTARIES for each M/N Value
 
 creek = "Perkins_creek"
 path = "/Users/bmelosh/VagrantBoxes/LSDTopoTools/Topographic_projects/Cache_creek/Previous_LSDTT_version/Channel_extraction/%s/" % creek#Set path to data
-name = creek+'_movern.csv'#name of movern file with all the chi data
+fname = creek+'_movern.csv'#name of movern file with all the chi data
 
 #Create a function to pull in MLE data
 mn_values = [x / 10.0 for x in range(1, 9, 1)]#create a list of floats.
 
 
 def group_chi_mle_data(creek, path, fname, mn_values):
-
     #Import Chi Data
     movern_df = pd.read_csv((path+name), delimiter=',', header=0, names=['source_key','basin_key','elevation','m_over_n = 0.1','m_over_n = 0.2','m_over_n = 0.3','m_over_n = 0.4','m_over_n = 0.5','m_over_n = 0.6','m_over_n = 0.7','m_over_n = 0.8'])
     movern_df = movern_df.rename(columns={'source_key': 'Tributary'})
     movern_df_tribs = movern_df[movern_df['Tributary']!=0]
     movern_df_ms = movern_df[movern_df['Tributary']==0]
 
-    #Import MLE Data and merge with Chi
+    #import MLE Data and merge with Chi
     mle_data_i = []
     mle_data_merge = pd.DataFrame()
     for mn in mn_values:
@@ -57,46 +56,21 @@ def group_chi_mle_data(creek, path, fname, mn_values):
     return movern_df_tribs_merge, movern_df_ms
 
 
-#Function to import CHI data for every M/N Value for each Tributary and Main Stem
-    
-def import_chi_data(path, creek, name):
-    
-    #Import Data
-    movern_df = pd.read_csv((path+name), delimiter=',', header=0, names=['source_key','basin_key','elevation','m_over_n = 0.1','m_over_n = 0.2','m_over_n = 0.3','m_over_n = 0.4','m_over_n = 0.5','m_over_n = 0.6','m_over_n = 0.7','m_over_n = 0.8'])
+tribs, main_stem = group_chi_mle_data(creek, path, fname, mn_values)
 
-    movern_df = creek_movern_df.rename(columns={'source_key': 'Tributary'})
-    
-    return movern_df
-
-
-
-
-
-
-#import chi data
-chi_data = import_chi_data(path, creek, name)
-
-#seperate into tribs and main stem
-chi = chi_data[chi_data['Tributary'] != 0]
-chi_ms = chi_data[chi_data['Tributary'] == 0]
-
-#import mle data
-tributaries_MLE = create_MLE_data(mn_values = [x / 10.0 for x in range(1, 9, 1)])
 
 #assign bokeh data sources
-chi_source = bp.ColumnDataSource(chi)
-chi_ms_source = bp.ColumnDataSource(chi_ms)
-mle_source = bp.ColumnDataSource(tributaries_MLE)
-
+souce_tribs = ColumnDataSource(tribs)
+source = ColumnDataSource(main_stem)
 
 
 # Set up plot
-plot = figure(plot_height=250, plot_width=500, title="m/n values",x_axis_label='Chi (X)', y_axis_label='Elevation (m)',
+plot = figure(plot_height=250, plot_width=500, title=creek, x_axis_label='Chi (X)', y_axis_label='Elevation (m)',
               tools="pan,reset,save,wheel_zoom")
-plot.circle('x', 'y', source=chi_source)
+plot.circle('m_over_n = 0.1', 'elevation', source=source_ms)
 
-
-# Set up widgets
+#show(plot)
+# # Set up widgets
 mn_values_slider = Slider(title="m/n values", value=0.0, start=0.1, end=0.8, step=0.1)
 
 
@@ -108,16 +82,16 @@ def update_data(attrname, old, new):
 
 
     # Generate the new curve
-    x = chi_source.data['m_over_n = %s' % mn]['chi']
-    y = chi_source.data['elevation']
+    x = source_ms.data['m_over_n = %s' % mn]['chi']
+    y = source_ms.data['elevation']
 
-    chi_source.data = dict(x=x, y=y)
+    source_ms.data = dict(x=x, y=y)
 
-for mn in [mn_values]:
+for mn in [mn_values_slider]:
     mn.on_change('value', update_data)
 
 # Set up layouts and add to document
-inputs = widgetbox(mn_values)
+inputs = widgetbox(mn_values_slider)
 
 curdoc().add_root(column(inputs, plot, width=800))
 curdoc().title = "m/n values"
