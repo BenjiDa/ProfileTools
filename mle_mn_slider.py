8fd4e475c36ab2585198633d1c131bb22f1c14e6
Benjamin L Melosh <bmelosh@usgs.gov>
2017-08-30 12:16:03 -0700

    time to go play basketball

--- mle_mn_slider.py
+++ mle_mn_slider.py
@@ -20,35 +20,41 @@ name = creek+'_movern.csv'#name of movern file with all the chi data
 mn_values = [x / 10.0 for x in range(1, 9, 1)]#create a list of floats.
 
 
-def create_MLE_data(mn_values):
-    TributariesMLE = []
-    mle_data_i = []
-    for mn in mn_values: 
-        num = mn
-        full_name = '%s%s_movernstats_%s_fullstats.csv' % (path, creek, num)
+def group_chi_mle_data(creek, path, fname, mn_values):
 
-        name_mle = 'MLE m/n %s' % num
-        name_rmse = 'RMSE m/n %s' % num
+    #Import Chi Data
+    movern_df = pd.read_csv((path+name), delimiter=',', header=0, names=['source_key','basin_key','elevation','m_over_n = 0.1','m_over_n = 0.2','m_over_n = 0.3','m_over_n = 0.4','m_over_n = 0.5','m_over_n = 0.6','m_over_n = 0.7','m_over_n = 0.8'])
+    movern_df = movern_df.rename(columns={'source_key': 'Tributary'})
+    movern_df_tribs = movern_df[movern_df['Tributary']!=0]
+    movern_df_ms = movern_df[movern_df['Tributary']==0]
+
+    #Import MLE Data and merge with Chi
+    mle_data_i = []
+    mle_data_merge = pd.DataFrame()
+    for mn in mn_values:
+
+        movern_df = pd.read_csv((path+name), delimiter=',', header=0, names=['source_key','basin_key','elevation','m_over_n = 0.1','m_over_n = 0.2','m_over_n = 0.3','m_over_n = 0.4','m_over_n = 0.5','m_over_n = 0.6','m_over_n = 0.7','m_over_n = 0.8'])
+        movern_df = movern_df.rename(columns={'source_key': 'Tributary'})
+        movern_df_tribs = movern_df[movern_df['Tributary']!=0]
+
+
+        full_name = '%s%s_movernstats_%s_fullstats.csv' % (path, creek, mn)
+
+        name_mle = 'MLE m/n %s' % mn
+        name_rmse = 'RMSE m/n %s' % mn
 
         mle_data = pd.read_csv(full_name, delimiter=',', header= 0,names=['basin_key', 'reference_source_key', 'test_source_key','MLE','RMSE'])
 
         mle_data = pd.DataFrame(mle_data)
         mle_data_i.append(mle_data)
 
-        creek_movern_tributaries_merge_i = creek_movern_df_tributaries.merge(mle_data, left_on = "Tributary", right_on = "test_source_key")
+        mle_data_merge['test_source_key'] = mle_data['test_source_key']
+        mle_data_merge[name_mle] = mle_data['MLE']
+        mle_data_merge[name_rmse] = mle_data['RMSE']
 
-        creek_movern_tributaries_merge[name_mle] = creek_movern_tributaries_merge_i['MLE']
-        creek_movern_tributaries_merge[name_rmse] = creek_movern_tributaries_merge_i['RMSE']
+        movern_df_tribs_merge = movern_df_tribs.merge(mle_data_merge, left_on = "Tributary", right_on = "test_source_key")
 
-        TributariesMLE_i = list(creek_movern_tributaries_merge[name_mle])
-
-        TributariesMLE.append(TributariesMLE_i)
-
-        tributaries_MLE = pd.DataFrame(TributariesMLE)
-        tributaries_MLE = tributaries_MLE.transpose()
-        tributaries_MLE.rename(columns=lambda x: 'm/n 0.%s' % x, inplace=True)#Name columns
-
-    return tributaries_MLE
+    return movern_df_tribs_merge, movern_df_ms
 
 
 #Function to import CHI data for every M/N Value for each Tributary and Main Stem
@@ -56,11 +62,11 @@ def create_MLE_data(mn_values):
 def import_chi_data(path, creek, name):
     
     #Import Data
-    creek_movern_df = pd.read_csv((path+name), delimiter=',', header=0, names=['source_key','basin_key','elevation','m_over_n = 0.1','m_over_n = 0.2','m_over_n = 0.3','m_over_n = 0.4','m_over_n = 0.5','m_over_n = 0.6','m_over_n = 0.7','m_over_n = 0.8'])
+    movern_df = pd.read_csv((path+name), delimiter=',', header=0, names=['source_key','basin_key','elevation','m_over_n = 0.1','m_over_n = 0.2','m_over_n = 0.3','m_over_n = 0.4','m_over_n = 0.5','m_over_n = 0.6','m_over_n = 0.7','m_over_n = 0.8'])
 
-    creek_movern_df = creek_movern_df.rename(columns={'source_key': 'Tributary'})
+    movern_df = creek_movern_df.rename(columns={'source_key': 'Tributary'})
     
-    return creek_movern_df
+    return movern_df
 
 
 
